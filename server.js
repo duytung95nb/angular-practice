23c81dc3acd84d83ffc411d442ffdbba0d96e8fd
Dao Tung <duytung95nb@gmail.com>
2019-09-24 17:10:51 +0700

    change path to built source

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  
 const app = express();  
-app.use(express.static(__dirname + '/dist'));  
+app.use(express.static(__dirname + '/dist/ivy-project/'));  
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/ivy-project/index.html');  
 });  
 app.listen(process.env.PORT || 8080);  
